b5311864a270d13c60ad2a3110bafc162e203a26
shamimsohail <168447871+shamimsohail@users.noreply.github.com>
2024-06-04 08:58:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//addition
+//let addition:number = 6+2;
+//console.log("addition",addition);
+//substraction
+//let subtraction:number = 10-2;
+//console.log("subtraction",subtraction);
+//multiplication
+//let multiplication:number = 4*2;
+//console.log("multiplication",multiplication); 
+//division
+//let division:number = 16/2;
+//console.log("division",division);
+// addition
+//console.log(6+2);
+//subtraction
+//console.log(10-2);
+//multiplication
+//console.log(4*2);
+//division
+//console.log(16/2);
+//favourite number
+var favouritenumber = 7;
+//print message
+console.log("My favourit number is ", (favouritenumber));
